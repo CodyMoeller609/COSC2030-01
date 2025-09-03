b74cff7c4797c5865aeff63e517ede3d84a82bed
Cody Moeller <codyjmoeller@gmail.com>
2025-09-02 18:46:40 -0600

    Program Funcitoning as Intended

--- folderOne/Addition.cpp
+++ folderOne/Addition.cpp
@@ -2,10 +2,24 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    cout << "Hello to the world" << endl;
-    cout << argv[1] << endl;
-    return 0;
+int AddUp(int A[ ], int n){
+    int sum = 0;
+    for(int i=0;i<(n-1);i++){
+        sum+=A[i];
+    }
+    return sum;
+}
 
+int main(int argc, char* argv[]) {
+    if(argc>11){
+        cout << "No more than 10 integers, please." << endl;
+    }else{
+        int AA[argc-1];
+        for(int i=0;i<argc-1;i++){
+            AA[i]=atoi(argv[i+1]);
+        }
+        cout << AddUp(AA, argc) << endl;
+        }
+    return 0;
 }
 
